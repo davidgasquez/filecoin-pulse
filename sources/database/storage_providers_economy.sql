7634ffd2dbc6a1369922237d1db0e639ce1bd375
David Gasquez <davidgasquez@gmail.com>
2024-10-02 08:22:00 +0200

    fix: 🐛 adjust query date range and ordering
    - Updated the date range in the `where` clause from '2023-01-01' to '2023-06-01'.
    - Changed the ordering of results to first sort by `provider_id` and then by `date` in descending order.

--- sources/database/storage_providers_economy.sql
+++ sources/database/storage_providers_economy.sql
@@ -8,5 +8,5 @@ from filecoin_daily_storage_providers_metrics
 where (balance is not null
    or locked_funds is not null
    or provider_collateral is not null)
-   and date >= '2023-01-01'
-order by date desc
+   and date >= '2023-06-01'
+order by provider_id desc, date desc
